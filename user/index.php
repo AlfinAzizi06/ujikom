3bfc42202cbb4288c9690d890122058414a85f76
AlfinAzizi06 <muhammadalfinazizi19@gmail.com>
2024-05-06 15:34:06 +0700

    first commit

---
+++ user/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../galeri/img/J.png" type="icon">
+    <title>Beranda | SMK 1 TRIPLE 'J'</title>
+</head>
+<body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand d-flex align-align-item" href="#">
+                <img src="pp.jpg" height="50" width="50" alt="logo"
+                    class="d-inline-block align-text-top me-3">
+                <div class="profile">
+                    <h4 class="my-0">SMK INDONESIA DIGITAL</h4>
+                    <P class="my-0">maju seiring perkembangan digital</P>
+                </div>
+            </a>
+        </div>
+    </nav>
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+          <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+<!-- Illustrations -->
+<div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-dark">PETA SEKOLAH</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="maps">
+                         <iframe src=
+                         "https://maps.google.com/maps?q=smks 1 triple j&t=&z=10&ie=UTF8&iwloc=&output=embed" width="100%" height="350" style="border:0;" allowfullscreen loading="lazy">
+                         #document 
+                         (https://www.google.com/maps/embed?origin=mfe&pb=!1m3!2m1!1ssmks+1+triple+j!6i10)
+                        </iframe>
+                        </div>
+                        <p>Besar harapan kami, sarana ini dapat memberi manfaat bagi semua pihak yang ada dilingkup pendidikan 
+                            dan pemerhati pendidikan secara khusus bagi SMK 1 Triple J.</p>
+                        <a target="_blank" rel="nofollow" href="https://smk1triplej.sch.id/">Browser SMK 1 TRIPLE J &rarr;</a>
+                    </div>
+                </div>
+
+                 <!-- Project Card Example -->
+                 <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-dark">4 Pilihan Kompetensi Keahlian SMK 1 TRIPLE "J"</h6>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="small font-weight-bold">REKAYASA PERANGKAT LUNAK <span
+                                class="float-right">90%</span></h4>
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 90%"
+                                aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <h4 class="small font-weight-bold">BIDANG DARING PEMASARAN<span
+                                class="float-right">90%</span></h4>
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 90%"
+                                aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <h4 class="small font-weight-bold">PERKANTORAN <span
+                                class="float-right">90%</span></h4>
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 90%"
+                                aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <h4 class="small font-weight-bold">AKUTANSI<span
+                                class="float-right">90%</span></h4>
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 90%"
+                                aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="col-lg-6 mb-4">
+
+            <!-- Approach -->
+            <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-dark">Visi dan Misi</h6>
+                    </div>
+                    <div class="card-body">
+                        <p>Assalamu'alaikum wr.wb.Puji syukur kepada Allah SWT, Tuhan Yang Maha Esa yang telah memberikan rahmat dan anugerahNya sehingga website SMK 1 Triple J ini dapat terbit. Salah satu tujuan dari website ini adalah untuk menjawab akan setiap kebutuhan informasi dengan memanfaatkan sarana teknologi informasi yang ada. 
+                           Kami sadar sepenuhnya dalam rangka memajukan pendidikan di era berkembangnya Teknologi Informasi yang begitu pesat, sangat diperlukan berbagai sarana prasarana yang kondusif, 
+                           kebutuhan berbagai informasi siswa, guru, orangtua maupun masyarakat, sehingga kami berusaha mewujudkan hal tersebut semaksimal mungkin. Semoga dengan adanya website ini dapat membantu dan bermanfaat,
+                           terutama informasi yang berhubungan dengan pendidikan, ilmu pengetahuan dan informasi seputar SMK 1 Triple J.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
